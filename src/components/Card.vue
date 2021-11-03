e41159e8026640ba9956cc04dc67694d5a6b1193
Lindsey Marie Wingate <lindseymariewingate@MacBook-Pro.local>
2021-11-02 17:18:53 -0700

    passed prop.

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-bind:id="background-color">
+	<p>{{ productName }}</p>	
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    productName: String
+  }
+}
+</script>
+
+<style scoped>
+.card {
+    background: red; 
+    border: 1px solid black;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <Card productName="backpack"></Card>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +32,14 @@
 </template>
 
 <script>
+import Card from './Card.vue'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    Card
   }
 }
 </script>
