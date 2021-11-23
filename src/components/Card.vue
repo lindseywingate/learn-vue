9c8a7d53e27b6c58b1763ac232157c781ceaed30
Lindsey Marie Wingate <lindseymariewingate@MacBook-Pro.local>
2021-11-22 16:53:49 -0800

    got stupid v-for working

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,41 @@
 <template>
-    <div v-bind:id="background-color">
-	<p>{{ productName }}</p>	
+    <div class="card">
+      <p>Thanksgiving Dinner</p>
+      <a v-on:click="log">Log Food</a>
+      <ul id="foodList">
+        <li v-for="item in food" :key="item.name">{{item.name}}</li>
+      </ul>
     </div>
 </template>
 
 <script>
+
 export default {
   name: 'Card',
-  props: {
-    productName: String
-  }
-}
+  methods: {
+    log() {
+    },
+  },
+  computed: {
+    currentDate() {
+      return Date.now();
+    }
+  },
+  data() {
+    return {
+      food: [
+        {name: 'turkey', course: 3},
+        {name: 'apple salad', course: 1},
+        {name: 'rice pilaf', course: 2}
+      ],
+    }
+  },
+};
 </script>
 
 <style scoped>
 .card {
-    background: red; 
-    border: 1px solid black;
+    background: #D6EAF8; 
+    border: 1px solid #5DADE2;
 }
 </style>
